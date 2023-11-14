5e87c7d5561987592647e98922afc4ae5d59f28d
Francisco Soto <sotofloresfrancisco@gmail.com>
2023-11-13 21:04:51 -0500

    comentando la funcion close-navbar

--- js/close-navbar.js
+++ js/close-navbar.js
@@ -1,7 +1,7 @@
-function closeNavbar() {
+/* function closeNavbar() {
     const navbar = document.querySelector('.navbar-collapse');
     if (navbar.classList.contains('show')) {
         navbar.classList.remove('show');
     }
 }
-document.addEventListener('click', closeNavbar);
+document.addEventListener('click', closeNavbar); */
