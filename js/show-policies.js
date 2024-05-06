1b1e37bb9301fa75084bb948a67204a75723bdcd
Francisco Soto <sotofloresfrancisco@gmail.com>
2024-05-06 15:11:59 -0500

    cambiando colores

--- js/show-policies.js
+++ js/show-policies.js
@@ -1,3 +1,10 @@
+let fechaActual = new Date();
+let anio = fechaActual.getFullYear();
+
+let anioActual = document.getElementById('anioActual');
+anioActual.textContent = anio;
+
 document.getElementById('mostrarPDF').addEventListener('click', function() {
         window.open('./files/politicas-de-privacidad-fiduto.pdf', '_blank');
 });
+
